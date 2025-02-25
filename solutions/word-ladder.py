858c0290de2b94de56b04fb78f00b18ccf29cea4
Madhuvanthi <107021946+Madhuvanthi-P-S@users.noreply.github.com>
2025-02-25 23:00:03 +0530

    Sync solutions: 2025-02-25 23:00:03

--- solutions/word-ladder.py
+++ solutions/word-ladder.py
@@ -1,22 +1,32 @@
-from collections import defaultdict
 class Solution:
     def ladderLength(self, beginWord: str, endWord: str, wordList: List[str]) -> int:
-
         hm= collections.defaultdict(list)
         wordSet = set(wordList)
-        for word in wordList:
-            for i in range(len(word)):
-                for c in 'abcdefghijklmnopqrstuvwxyz':
-                    neiWord = word[:i]+c+word[i+1:]
-                    if neiWord in wordSet and neiWord!=word:
-                        hm[word].append(neiWord)
-        res=0
 
-        q=hm[beginWord]
-        print(q)
-        while(q):
-            for i in range(len(q)):
-                neiW = q.popleft()
-                
+        def build_adj_list(wordList):
+            for word in wordList:
+                for i in range(len(word)):
+                    for c in 'abcdefghijklmnopqrstuvwxyz':
+                        neiWord = word[:i]+c+word[i+1:]
 
+                        if (neiWord in wordSet and neiWord!=word):
+                            hm[word].append(neiWord)
+            return hm
+
+        adj_list = build_adj_list([beginWord]+ wordList)
+        print(adj_list)
         
+        q = deque([(beginWord,1)])
+        visited=set()
+        while(q):
+            currWord , step = q.popleft()
+
+            if currWord == endWord:
+                return step
+            for neiW in adj_list.get(currWord,[]):
+                if neiW not in visited:
+                    visited.add(neiW)
+                    q.append((neiW,step+1))
+        if endWord not in wordSet:
+            return 0
+        return step
