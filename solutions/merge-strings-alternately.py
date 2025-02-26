01c99b08c36743c9cb9b1de82659a3126f1cff42
Madhuvanthi <107021946+Madhuvanthi-P-S@users.noreply.github.com>
2025-02-26 08:00:02 +0530

    Sync solutions: 2025-02-26 08:00:02

--- solutions/merge-strings-alternately.py
+++ solutions/merge-strings-alternately.py
@@ -1,10 +1,10 @@
 class Solution:
     def mergeAlternately(self, word1: str, word2: str) -> str:
-        res=""
+
+        res=''
         for i in range(min(len(word1),len(word2))):
             res+=word1[i]+word2[i]
-        res+=word1[min_len:] + word2[min_len:]
-    
-            
-        return res
+        res+=word1[i+1:]
+        res+=word2[i+1:]
         
+        return res
