21c349c97b108be6e68136de1c9ac5e64491a428
Madhuvanthi <107021946+Madhuvanthi-P-S@users.noreply.github.com>
2025-03-15 15:00:02 +0530

    Sync solutions: 2025-03-15 15:00:02

--- solutions/longest-palindromic-substring.py
+++ solutions/longest-palindromic-substring.py
@@ -1,12 +1,31 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        substring=[]
-        longestSubstr , res = 0 , ''
-        for i in range(len(s)):
-            for j in range(i+1,len(s)+1):
-                if s[i:j+1] == s[i:j+1][::-1]:
-                    if(len(s[i:j+1])>len(res)):
-                        longestSubstr = max(longestSubstr,len(s[i:j+1]))
-                        res=s[i:j+1]
-        return res
-    
+        t = s[::-1]  # Reverse the string
+        n = len(s)
+        
+        # Create DP table
+        dp = [[0 for i in range(n+1)] for j in range(n+1)]
+        
+        # Fill DP table
+        for i in range(n-1, -1, -1):
+            for j in range(n-1, -1, -1):
+                if s[i] == t[j]:
+                    dp[i][j] = 1 + dp[i+1][j+1]
+                else:
+                    dp[i][j] = max(dp[i+1][j], dp[i][j+1])
+        
+        # Reconstruct the longest palindromic subsequence from the DP table
+        i, j = 0, 0
+        lps = []
+        while i < n and j < n:
+            if s[i] == t[j]:
+                lps.append(s[i])
+                i += 1
+                j += 1
+            elif dp[i+1][j] >= dp[i][j+1]:
+                i += 1
+            else:
+                j += 1
+        
+        # Return the subsequence as a string
+        return ''.join(lps)
--- solutions/substrings-of-size-three-with-distinct-characters.py
+++ solutions/substrings-of-size-three-with-distinct-characters.py
@@ -1,7 +1,9 @@
 class Solution:
     def countGoodSubstrings(self, s: str) -> int:
         count = 0
-        for i in range(len(s) - 2):
-            if len(set(s[i:i+3])) == 3:  # Check if all characters are unique
+
+        for i in range(len(s) - 2):  # Sliding window of size 3
+            if s[i] != s[i+1] and s[i+1] != s[i+2] and s[i] != s[i+2]:  # All distinct
                 count += 1
+        
         return count
