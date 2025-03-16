29a0a2058d337d17c30432488e1c9525985ab337
Madhuvanthi <107021946+Madhuvanthi-P-S@users.noreply.github.com>
2025-03-17 04:00:03 +0530

    Sync solutions: 2025-03-17 04:00:03

--- solutions/minimum-difference-between-highest-and-lowest-of-k-scores.py
+++ solutions/minimum-difference-between-highest-and-lowest-of-k-scores.py
@@ -3,7 +3,7 @@ class Solution:
         
         nums.sort()
         res = float('inf')
-        if(len(nums)==1):
+        if(len(nums)==1 or k==1):
             return 0
         for i in range(len(nums)-k+1):
             res = min(res,nums[i+k-1]-nums[i])
